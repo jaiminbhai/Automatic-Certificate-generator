b0a5db727d025ee12f09c04dd9240820076f09e5
jaiminbhai <121561686+jaiminbhai@users.noreply.github.com>
2024-09-11 23:44:22 +0530

    Add files via upload

---
+++ Automatic_certificate_gen.py
@@ -0,0 +1,20 @@
+# importing packages & modules
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd 
+import os 
+
+# impiementation to generate certificate
+df = pd.read_csv('list.csv')
+font = ImageFont.truetype('arial.ttf', 60)
+for index, j in df.iterrows():
+    img = Image.open('certification.png')
+    draw = ImageDraw.Draw(img)
+    draw.text(xy = (150,250),
+text = '{}'.format(j['name']),
+fill = (0,0,0),
+font =font)
+img.save('pictures/{}.png'.format(j['name']))
+
+
+
+
